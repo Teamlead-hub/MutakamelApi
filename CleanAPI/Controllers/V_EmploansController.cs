c6e191d2b9da4390bc7c3fe36053004c8be242b3
marwacodenetlabs <marwa@codenetlabs.com>
2025-10-21 17:00:32 +0300

    ____________________

---
+++ CleanAPI/Controllers/V_EmploansController.cs
@@ -0,0 +1,9 @@
+﻿using CleanBase.Entities;
+using CleanBusiness.Declarations;
+
+namespace CleanAPI.Controllers
+{
+    public class V_EmploansController(IV_Emploans service) : BaseController<V_Emploans>(service)
+    {
+    }
+}
--- CleanAPI/Program.cs
+++ CleanAPI/Program.cs
@@ -339,6 +339,7 @@ namespace CleanAPI
             builder.Services.AddScoped<ILeavesDetails, LeavesDetailsService>();
 
             builder.Services.AddScoped<IEmploans, EmploansService>();
+            builder.Services.AddScoped<IV_Emploans, V_EmploansService>();
 
             builder.Services.AddScoped<IV_IncrementMain, V_IncrementMainService>();
             builder.Services.AddScoped<IV_IncrementDetails, V_IncrementDetailsService>();
@@ -1021,6 +1022,7 @@ namespace CleanAPI
             builder.EntitySet<LeavesDetails>("LeavesDetails");
 
             builder.EntitySet<Emploans>("Emploans");
+            builder.EntitySet<V_Emploans>("V_Emploans");
 
             builder.EntitySet<V_IncrementMain>("V_IncrementMain");
             builder.EntitySet<V_IncrementDetails>("V_IncrementDetails");
